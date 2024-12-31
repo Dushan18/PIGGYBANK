8659f57c1b2372ceac5b412f1fc8cc2aa80a62c1
Tu Nombre <tu.email@example.com>
2024-12-31 03:15:15 -0500

    Actulizado_31/12/2024

--- src/app/shared/ui/navbar/navbar.component.ts
+++ src/app/shared/ui/navbar/navbar.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
 
+  constructor(private router: Router) {}
+
+  isActive(route: string): boolean {
+    return this.router.url === route;
+  }
 }
